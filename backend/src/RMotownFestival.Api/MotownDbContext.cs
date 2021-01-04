723716e27ef19c6f5f7c3435728bcb8ee0f45e84
koen.loyens@realdolmen.com <koen.loyens@realdolmen.com>
2021-01-04 09:58:51 +0100

    commit alles

---
+++ backend/src/RMotownFestival.Api/MotownDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RMotownFestival.Api.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RMotownFestival.Api
+{
+    public class MotownDbContext : DbContext
+    {
+        public MotownDbContext(DbContextOptions options) : base(options)
+        { 
+        
+        }
+
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Stage> Stages { get; set; }
+
+      
+    }
+}
--- backend/src/RMotownFestival.Api/Startup.cs
+++ backend/src/RMotownFestival.Api/Startup.cs
@@ -1,6 +1,7 @@
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,7 +26,11 @@ namespace RMotownFestival.Api
 
             services.AddCors();
             services.AddControllers();
+
+            services.AddDbContext<MotownDbContext>(
+                options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
